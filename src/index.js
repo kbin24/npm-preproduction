3bcf8e5d567b118f8429412bf4e98b674abffb55
DESKTOP-S1E7OB9\kbin <kbin_xiaobin@163.com>
2022-06-11 17:03:43 +0800

    直接读取本地文件内容返回

--- src/index.js
+++ src/index.js
@@ -4,22 +4,61 @@ const path = require('path')
 const url = require('url')
 const fs = require('fs')
 const { hostname } = require('os')
-const localServer = http.createServer()
 const httpServer = http.createServer()
 
-//本地服务用来访问文件
-localServer.on('request', (req, res) => {
-    //当前的工作目录
+//走http转发
+const config = {
+    "www.bin11.com": "localhost:8080",
+    "192.168.31.247:5000": "192.168.31.247:5001"
+}
+httpServer.on('request', (req, res) => {
+    const { href, host, pathname, hash, search } = new URL(req.url)
+    const { method, headers } = req
+
+    let url = href
+    let [StateUrl, ResouceUrl] = Object.keys(config)
+
+    //如果访问的是静态资源 直接从本地文件读取
+    if (StateUrl.indexOf(host) != -1) {
+        readLoacalFile(req, res)
+    } else if (ResouceUrl.indexOf(host) != -1) {
+        url = 'http://' + config[host]
+    }
+    const newUrl = new URL(url)
+    const { hostname, protocol, port } = newUrl
+    let options = {
+        protocol,
+        hostname,
+        method,
+        port: port || 80,
+        path: pathname + hash + search,
+        headers: {
+            ...headers,
+            host: hostname
+        }
+    }
+    // delete options.headers['accept-encoding']
+    const proxyRequest = http.request(options, proxyRes => {
+        res.writeHead(proxyRes.statusCode, proxyRes.headers)
+        proxyRes.pipe(res)
+    })
+    req.pipe(proxyRequest)
+})
+
+httpServer.listen(7700,()=>{
+    console.log(`请配置代理地址为: http://${hostname}:${7700}`)
+})
+
+
+//读取本地文件
+const readLoacalFile = (req, res) => {
+    //当前执行命令的目录
     const basePath = process.cwd()
-    //请求链接
+    //请求url
     const pathObj = url.parse(req.url, true)
     const pathname = pathObj.pathname === '/' ? '/index.html' : pathObj.pathname
     //文件路径
     const filePath = path.join(basePath, pathname)
-    /*     //同步读取数据
-        let fileContent = fs.readFileSync(filePath,'binary')
-        res.write(fileContent,'binary')
-        res.end() */
     //异步读取文件数据
     fs.readFile(filePath, 'binary', (err, data) => {
         //防止读取文件报错时服务器崩掉
@@ -32,51 +71,4 @@ localServer.on('request', (req, res) => {
             res.end()
         }
     })
-})
-
-localServer.listen(8080, () => {
-    console.log('8080')
-})
-
-
-
-//走http转发
-const config = {
-    "www.bin11.com": "localhost:8080",
-    "192.168.31.247:5000": "192.168.31.247:5001"
-} 
-httpServer.on('request',(req,res)=>{
-    const {href,host,pathname,hash,search} = new URL(req.url)
-    const {method, headers} = req
-
-    let url = href
-    let [StateUrl, ResouceUrl] = Object.keys(config)
-    Object.keys(config)[0].indexOf(host)
-    if(StateUrl.indexOf(host) != -1 || ResouceUrl.indexOf(host) != -1){
-        url = 'http://' + config[host]
-    }
-
-    const newUrl = new URL(url)
-    const {hostname, protocol, port} = newUrl
-     let options = {
-        protocol,
-        hostname,
-        method,
-        port: port || 80,
-        path: pathname + hash + search,
-        headers: {
-            ...headers,
-            host: hostname
-        }
-    }
-    // delete options.headers['accept-encoding']
-    const proxyRequest = http.request(options,proxyRes=>{
-        res.writeHead(proxyRes.statusCode,proxyRes.headers)
-        proxyRes.pipe(res)
-    })
-    req.pipe(proxyRequest)
-})
-
-httpServer.listen(3003,()=>{
-    console.log(`${hostname} server start 3003`)
-})
+}
